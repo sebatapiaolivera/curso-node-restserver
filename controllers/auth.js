84056c34e1d85a3947aeda8827ec2002f96ff81d
Sebastian Tapia Olivera <seba.tapia.olivera@gmail.com>
2022-11-18 12:00:30 -0300

    CHG: Se implementa google sign in.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,7 +1,8 @@
-const { response } = require('express');
+const { response, json } = require('express');
 const Usuario = require('../models/usuario')
 const bcryptjs = require('bcryptjs');
 const { generarJWT } = require('../helpers/generar-jwt');
+const { googleVerify } = require('../helpers/google-verify');
 
 const login = async(req, res = response) => {
 
@@ -47,6 +48,49 @@ const login = async(req, res = response) => {
   }
 };
 
+
+const googleSignIn = async(req, res = response) => {
+  const { id_token } = req.body;
+
+  try {
+    const {correo, nombre, img} = await googleVerify(id_token);
+
+    let usuario = await Usuario.findOne({ correo });
+
+    if (!usuario) {
+      //tengo que crearlo
+      const data = {
+        nombre,
+        correo,
+        password: '',
+        img,
+        google: true
+      };
+      usuario = new Usuario(data);
+      await usuario.save();
+    }
+
+    //si el usuario en BD esta en false el estado
+    if (!usuario.estado) {
+      return res.status(401).json({
+        msg: 'Hable con el administrador, usuario bloqueado'
+      });
+    }
+
+    const token = await generarJWT(usuario.id);
+
+    res.json({
+      usuario,
+      token
+    })
+  } catch (error) {
+    res.status(400).json({
+      ok: false,
+      msg: 'El token no se pudo verificar.'
+    })
+  }
+}
 module.exports = {
-  login
+  login,
+  googleSignIn
 }
